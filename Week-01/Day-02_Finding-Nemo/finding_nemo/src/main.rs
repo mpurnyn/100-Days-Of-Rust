6836fe2e4babe75ab8cdf374f7e71fb0e2b7cc56
Peter Purnyn <mpurnyn@gmail.com>
2026-08-10 14:19:15 -0500

    day 2

---
+++ Week-01/Day-02_Finding-Nemo/finding_nemo/src/main.rs
@@ -0,0 +1,17 @@
+fn find_nemo(str: &str) -> String {
+    let v: Vec<&str> = str.split(' ').collect();
+    for (i, word) in v.iter().enumerate() {
+        if *word == "Nemo" {
+            let pos = i + 1;
+            return format!("I found Nemo at {}!", pos);
+        }
+    }
+    return format!("I can't find Nemo :(");
+}
+
+fn main() {
+    assert_eq!(find_nemo("I am finding Nemo !"), "I found Nemo at 4!");
+    assert_eq!(find_nemo("Nemo is me"), "I found Nemo at 1!");
+    assert_eq!(find_nemo("I Nemo am"), "I found Nemo at 2!");
+}
+
